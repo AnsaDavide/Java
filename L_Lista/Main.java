28cc5e5c19715e58848661caf7968b2f23c16f28
AnsaDavide <150423454+AnsaDavide@users.noreply.github.com>
2024-03-14 12:26:55 +0000

    Modifiche

--- L_Lista/Main.java
+++ L_Lista/Main.java
@@ -8,10 +8,18 @@ public class Main {
         Lista l = new Lista();
         System.out.println( l );
 
+        /* 
         for (int i=0; i<10; i++) {
-            l.add( new Nodo( i, null ));
+            l.addHead( new Nodo( i, null ));
         }
+        l.addHead(new Nodo(2024, null));
+        */
 
+        l.addSorted(new Nodo(300, null));
+        l.addSorted(new Nodo(320, null));
+        l.addSorted(new Nodo(250, null));
+        l.addSorted(new Nodo(180, null));
+        l.addSorted(new Nodo(350, null));
         System.out.println( l );
     } 
 
